e0a2a3f2be27272d6eaf7bda99d3df907519b996
dongpohezui <40270581+dongpohezui@users.noreply.github.com>
2019-08-20 15:40:53 +0800

    Add files via upload

---
+++ noMaxLength.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var inputs = document.getElementsByTagName('input');
+var i;
+
+for (i=0; i<inputs.length; i++) {
+	inputs[i].setAttribute("maxlength","");
+}
+
+var inputs = document.getElementsByTagName('input');
+var i;
+
+for (i=0; i<inputs.length; i++) {
+	inputs[i].setAttribute("maxlength","");
+}
